cd53d518ef307fcc262d511d62b707f4a0f910a8
dominic <104272130+DominicBennett@users.noreply.github.com>
2023-05-17 17:56:12 +0100

    add lists of words to use in movie name generator

---
+++ pulpMovieNames.js
@@ -0,0 +1,3 @@
+const adjective = ['cannibal', 'vampire', 'zombie', 'killer', 'flesh-eating', 'man-eating', 'mutant', 'giant', 'radioactive'];
+const creatures = ['hamster', 'gerbils', 'leopards', 'chihuahuas', 'rodents', 'spiders', 'slugs', 'corpses', 'ghouls', 'latin teachers', 'dominatrixes'];
+const origins = ['Mars', 'beyond the grave', 'Venus', 'the 5th dimension', 'beyond time', 'Hell', 'the future'];
