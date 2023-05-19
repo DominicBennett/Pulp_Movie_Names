d05fa32a013e938696986c34b98a309a3a719389
dominic <104272130+DominicBennett@users.noreply.github.com>
2023-05-19 14:19:44 +0100

    use getRandomWord to genertae a movie name, log it to console

--- pulpMovieNames.js
+++ pulpMovieNames.js
@@ -1,9 +1,12 @@
 const adjective = ['cannibal', 'vampire', 'zombie', 'killer', 'flesh-eating', 'man-eating', 'mutant', 'giant', 'radioactive'];
-const creatures = ['hamster', 'gerbils', 'leopards', 'chihuahuas', 'rodents', 'spiders', 'slugs', 'corpses', 'ghouls', 'latin teachers', 'dominatrixes'];
-const origins = ['Mars', 'beyond the grave', 'Venus', 'the 5th dimension', 'beyond time', 'Hell', 'the future'];
+const creatures = ['hamsters', 'gerbils', 'leopards', 'chihuahuas', 'rodents', 'spiders', 'slugs', 'corpses', 'ghouls', 'latin teachers', 'dominatrixes'];
+const origins = ['Uranus', 'Mars', 'beyond the grave', 'Venus', 'the 5th dimension', 'beyond time', 'Hell', 'the future'];
 
 const getRandomWord = array => {
     return array[Math.floor(Math.random()*array.length)]
     };
+    
+let movieName = `${getRandomWord(adjective)} ${getRandomWord(creatures)} from ${getRandomWord(origins)}.`;
 
+console.log(movieName.toUpperCase());
 
