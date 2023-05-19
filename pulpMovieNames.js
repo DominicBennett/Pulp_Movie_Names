28ba67455d7c4a8715818414c9422859aaf283f7
dominic <104272130+DominicBennett@users.noreply.github.com>
2023-05-19 14:59:21 +0100

    include a loop to generate 10 random movie names and log to console

--- pulpMovieNames.js
+++ pulpMovieNames.js
@@ -4,21 +4,13 @@ const wordLists = {
     origins: ['Uranus', 'Mars', 'beyond the grave', 'Venus', 'the 5th dimension', 'beyond time', 'Hell', 'the future']
 }
 
-
-
-
-
-/*const adjective = ['cannibal', 'vampire', 'zombie', 'killer', 'flesh-eating', 'man-eating', 'mutant', 'giant', 'radioactive'];
-const creatures = ['hamsters', 'gerbils', 'leopards', 'chihuahuas', 'rodents', 'spiders', 'slugs', 'corpses', 'ghouls', 'latin teachers', 'dominatrixes'];
-const origins = ['Uranus', 'Mars', 'beyond the grave', 'Venus', 'the 5th dimension', 'beyond time', 'Hell', 'the future'];
-*/
-
-
 const getRandomWord = array => {
     return array[Math.floor(Math.random()*array.length)]
     };
     
-let movieName = `${getRandomWord(wordLists.adjective)} ${getRandomWord(wordLists.creatures)} from ${getRandomWord(wordLists.origins)}.`;
 
-console.log(movieName.toUpperCase());
+for (let i = 0; i < 10; i++) {
+    let movieName = `${getRandomWord(wordLists.adjective)} ${getRandomWord(wordLists.creatures)} from ${getRandomWord(wordLists.origins)}.`;
+    console.log(movieName.toUpperCase());
+    };
 
