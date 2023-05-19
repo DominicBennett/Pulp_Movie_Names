430e70e14fe37aedd44165d0f6ca659acb0eb449
dominic <104272130+DominicBennett@users.noreply.github.com>
2023-05-19 14:29:02 +0100

    store arrays in an object instead of seperate variables

--- pulpMovieNames.js
+++ pulpMovieNames.js
@@ -1,6 +1,18 @@
-const adjective = ['cannibal', 'vampire', 'zombie', 'killer', 'flesh-eating', 'man-eating', 'mutant', 'giant', 'radioactive'];
+const wordLists = {
+    adjective: ['cannibal', 'vampire', 'zombie', 'killer', 'flesh-eating', 'man-eating', 'mutant', 'giant', 'radioactive'],
+    creatures: ['hamsters', 'gerbils', 'leopards', 'chihuahuas', 'rodents', 'spiders', 'slugs', 'corpses', 'ghouls', 'latin teachers', 'dominatrixes'],
+    origins: ['Uranus', 'Mars', 'beyond the grave', 'Venus', 'the 5th dimension', 'beyond time', 'Hell', 'the future']
+}
+
+
+
+
+
+/*const adjective = ['cannibal', 'vampire', 'zombie', 'killer', 'flesh-eating', 'man-eating', 'mutant', 'giant', 'radioactive'];
 const creatures = ['hamsters', 'gerbils', 'leopards', 'chihuahuas', 'rodents', 'spiders', 'slugs', 'corpses', 'ghouls', 'latin teachers', 'dominatrixes'];
 const origins = ['Uranus', 'Mars', 'beyond the grave', 'Venus', 'the 5th dimension', 'beyond time', 'Hell', 'the future'];
+*/
+
 
 const getRandomWord = array => {
     return array[Math.floor(Math.random()*array.length)]
