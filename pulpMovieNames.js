7207ceac577019f320e633ecbd9acefcbb5775c6
dominic <104272130+DominicBennett@users.noreply.github.com>
2023-05-19 14:04:50 +0100

    add function to return random element from supplied array

--- pulpMovieNames.js
+++ pulpMovieNames.js
@@ -1,3 +1,9 @@
 const adjective = ['cannibal', 'vampire', 'zombie', 'killer', 'flesh-eating', 'man-eating', 'mutant', 'giant', 'radioactive'];
 const creatures = ['hamster', 'gerbils', 'leopards', 'chihuahuas', 'rodents', 'spiders', 'slugs', 'corpses', 'ghouls', 'latin teachers', 'dominatrixes'];
 const origins = ['Mars', 'beyond the grave', 'Venus', 'the 5th dimension', 'beyond time', 'Hell', 'the future'];
+
+const getRandomWord = array => {
+    return array[Math.floor(Math.random()*array.length)]
+    };
+
+
